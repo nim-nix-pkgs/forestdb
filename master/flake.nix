a7ca732ee39c431a08f4f0c345ae7b9bde959522
hugosenari <hugosenari@gmail.com>
2022-03-27 18:16:09 -0300

    feat(nimpkgs): remove lock

--- master/flake.nix
+++ master/flake.nix
@@ -11,6 +11,7 @@
   inputs.src-forestdb-master.ref   = "refs/heads/master";
   inputs.src-forestdb-master.owner = "nimscale";
   inputs.src-forestdb-master.repo  = "forestdb";
+  inputs.src-forestdb-master.dir   = "";
   inputs.src-forestdb-master.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
